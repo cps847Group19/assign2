de58a809bf779fca28be681ba1f5965b49aeba8f
jpatel20 <jay_patel20@hotmail.com>
2018-02-24 17:56:47 -0500

    Update files

--- my_functions.py
+++ my_functions.py
@@ -1,4 +1,4 @@
-def multliply_by_two(x):
+def multiply_by_two(x):
     return x*2
 
 def multiply_by_ten(x):
--- test.py
+++ test.py
@@ -6,11 +6,11 @@ class TestMyFunc(unittest.TestCase):
     def setUp(self):
         pass 
 
-    def test_multliply_by_two(self): 
-        self.assertEqual(  my_functions.multliply_by_two(4), 8)
+    def test_multiply_by_two(self): 
+        self.assertEqual(  my_functions.multiply_by_two(4), 8)
 
-    def test_multliply_by_ten(self):
-        self.assertEqual( my_functions.multliply_by_ten(10), 100)
+    def test_multiply_by_ten(self):
+        self.assertEqual( my_functions.multiply_by_ten(10), 100)
 
 if __name__ == '__main__':
     unittest.main()
