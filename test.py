e6714182353d4e437182c2620a146dd96bc10ad1
Jay Patel <jpatel20@users.noreply.github.com>
2018-02-24 17:51:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import my_functions
 class TestMyFunc(unittest.TestCase):
  
     def setUp(self):
-        pass
- 
-    def test_increment_one_1(self): 
-        self.assertEqual(  multliply_by_two(12), 24)
-  
-    def test_increment_one_2(self):
-        self.assertEqual( my_functions. multliply_by_ten(10), 100)
+        pass 
+
+    def test_multliply_by_two(self): 
+        self.assertEqual(  my_functions.multliply_by_two(4), 8)
+
+    def test_multliply_by_ten(self):
+        self.assertEqual( my_functions.multliply_by_ten(10), 100)
 
 if __name__ == '__main__':
     unittest.main()
